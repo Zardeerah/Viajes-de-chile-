d75e673ddcb7c00f260affd0b7934b7586baecb9
Paola <paola.stuardo.viveros@gmail.com>
2024-07-03 10:14:11 -0400

    ultimos arreglos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,33 @@
-<script>
-  $(document).ready(function() {
-    // Smooth scroll para los enlaces internos
-    $("a").on('click', function(event) {
-      if (this.hash !== "") {
-        event.preventDefault();
 
-        var hash = this.hash;
+$(document).ready(function () {
+$("button").tooltip()
 
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top
-        }, 800, function(){
 
-          window.location.hash = hash;
-        });
-      }
-    })
-  });
-</script>
+
+  // Smooth scroll para los enlaces internos
+  $("a").on('click', function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function () {
+
+        window.location.hash = hash;
+      });
+    }
+  })
+
+
+  
+
+
+
+});
+
+
+
+
+
